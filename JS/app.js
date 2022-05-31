8924a1be16f785523102e60eb113e198ce712fbc
Sang0920 <2001203004@hufi.edu.vn>
2022-05-31 23:59:43 +0700

    edited my own js file!

--- JS/app.js
+++ JS/app.js
@@ -7,3 +7,77 @@ selector(".menu").addEventListener("click", function(){
 function selector(s){
     return document.querySelector(s);
 }
+
+/* --> script for login page*/
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+    document.querySelector("#linkCreateAccount").addEventListener("click", e =>{
+        e.preventDefault();
+        loginForm.classList.add("form-hidden");
+        createAccountForm.classList.remove("form-hidden");
+    })
+    document.querySelector("#linkLogin").addEventListener("click", e =>{
+        e.preventDefault();
+        loginForm.classList.remove("form-hidden"); 
+        createAccountForm.classList.add("form-hidden");
+    })
+})
+function login(){
+    alert("Successful Login!");
+}
+function signup(){
+    alert("Your account is created!");
+}
+/* script for login page <--*/
+
+
+/* ---> Script for product list page*/
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+/* Script for prodcut list page <-- */
